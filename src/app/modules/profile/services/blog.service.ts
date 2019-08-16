e4c02b6eb7c91bfbd57feb842896777413f7f4bb
sdpsarker88 <sdpsarker88@gmail.com>
2019-08-17 03:19:10 +0600

    authToken header

--- src/app/modules/profile/services/blog.service.ts
+++ src/app/modules/profile/services/blog.service.ts
@@ -1,17 +1,23 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {BlogModel} from '../models/profile.model';
 import {Observable} from 'rxjs';
+import {UserService} from './user.service';
+import {AuthService} from '../../../core/components/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient,
+              private authService: AuthService) {}
 
   getAllBlogPosts(url): Observable<BlogModel[]> {
-    return this.httpClient.get<BlogModel[]>(url);
+    let headers: HttpHeaders = new HttpHeaders();
+    const authToken = this.authService.loadToken();
+    headers = headers.append('Authorization', authToken);
+    return this.httpClient.get<BlogModel[]>(url, {headers});
   }
 
   saveBlogPost(url, data: any): Observable<BlogModel> {
